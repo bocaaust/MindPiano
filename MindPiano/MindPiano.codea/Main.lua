adfb16a76fbcf35a3ec02c53cb0173dd0be59c98
Austin Lubetkin <bocaaust@users.noreply.github.com>
2016-12-31 19:29:05 -0500

    Touch Culling

--- MindPiano/MindPiano.codea/Main.lua
+++ MindPiano/MindPiano.codea/Main.lua
@@ -16,6 +16,8 @@ function setup()
    --samples[1] = {"Dropbox:c1","Dropbox:c4","Dropbox:c8"}
     rectMode(CORNER)
     touches = {}
+    touches[1] = {}
+    touches[2] = {}
     isBlink = false
     frequency = {4168.01,3951.07,3729.31,3520.00,3322.44,3135.96,2959.96,2793.83,2637.02,2489.02,2349.32,2217.46,2093.00,1975.53,1864.66,1760.00,1661.22,1567.98,1479.98,1396.91,1318.51,1244.51,1174.66,1108.73,1046.50,987.767,932.328,880.000,830.609,783.991,739.989,698.456,659.255,622.254,587.330,554.365,523.251,493.883,466.164,440.000,415.305,391.995,369.994,349.228,329.628,311.127,293.665,277.183,261.626,246.942,233.082,220.000,207.652,195.998,184.997,174.614,164.814,155.563,146.832,138.591,130.813,123.471,116.541,110.000,103.826,97.9989,92.4986,87.3071,82.4069,77.7817,73.4162,69.2957,65.4064,61.7354,58.2705,55.0000,51.9131,48.9994,46.2493,43.6535,41.2034,38.8909,36.7081,34.6478,32.7032,30.8677,29.1352,27.5000}
     keyType = {false,true,false}
@@ -57,17 +59,19 @@ function setup()
     
 end
 
+
 function touched(touch)
-    if touch.state == ENDED then
+    if touch.state == ENDED or os.didifftime(os.time(),touches[2][touch.id]) > 3 then
         -- When any touch ends, remove it from
         --  our table
-        drawKey5(touch)
-        touches[touch.id] = nil
+        touches[1][touch.id] = nil
+        touches[2][touch.id] = nil
     else
         -- If the touch is in any other state
         --  (such as BEGAN) we add it to our
         --  table
-        touches[touch.id] = touch
+        touches[1][touch.id] = touch
+        touches[2][touch.id] = os.time()
     end
 end
 
@@ -93,10 +97,10 @@ function draw()
     fill(0)
     rect(0,HEIGHT/6,WIDTH,HEIGHT/6+HEIGHT/2)
     tint(255)
-    for k,touch in pairs(touches) do
+    for k,touch in pairs(touches[1]) do
         drawKey5(touch)
     end
-    if #touches ==0 then
+    if #touches[1] ==0 then
         selected = backup
         drawKey5(CurrentTouch)
     end
